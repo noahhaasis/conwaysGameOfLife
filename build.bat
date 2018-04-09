1df749fd729f273927fa9e66c1522babbd340cf1
Noah Haasis <haasis_noah@yahoo.de>
2018-04-09 14:22:36 +0200

    Delete build.bat

--- build.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-pushd build
-cl /Z7 /MT -nologo /W4 ..\conways_game_of_life.c ..\board.c /I "c:\development\sdl2\include" -link /subsystem:console  /DYNAMICBASE "SDL2.lib" "SDL2main.lib"  /LIBPATH:"C:\Development\SDL2\lib\x86"
-popd build
-

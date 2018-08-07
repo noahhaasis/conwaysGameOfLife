1cc3089c4e9a9057e6665b324906ed4035723cc4
Noah Haasis <haasis_noah@yahoo.de>
2018-08-07 19:48:14 +0200

    Correct a comment

--- conways_game_of_life.c
+++ conways_game_of_life.c
@@ -235,7 +235,7 @@ int main(int argc, char** argv)
         {
             int cursor_x, cursor_y;
             SDL_GetGlobalMouseState( &cursor_x, &cursor_y );
-            // change the cell state if the mouse position changed
+            // change the cell state if the mouse points to a new cell
             if ( !( cursor_x / player_view.cell_size == mouse.last_cursor_x / player_view.cell_size && 
                     cursor_y / player_view.cell_size == mouse.last_cursor_y / player_view.cell_size ) )
             {
